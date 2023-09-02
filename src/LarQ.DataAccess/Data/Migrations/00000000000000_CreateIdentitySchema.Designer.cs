e3eb4ae236cf2c9f02453331a7c84d94ccfbd528
Muhammed Hamdy <modclover7@gmail.com>
2023-09-02 20:56:42 +0300

    Initial commit

--- src/LarQ.DataAccess/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ src/LarQ.DataAccess/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -2,6 +2,7 @@
 
 using System;
 using LarQ.Data;
+using LarQ.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
@@ -13,7 +14,8 @@ namespace LarQ.Data.Migrations
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        // protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
